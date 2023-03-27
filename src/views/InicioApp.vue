3fcef1cdc938f46def2ef4d97364eed5d92da543
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-03-27 11:52:16 +0200

    Landing Sin Botones

--- src/views/InicioApp.vue
+++ src/views/InicioApp.vue
@@ -4,6 +4,12 @@
     <section class="secIni1">
       <img src="../assets/imInicioUsoWeb.jpg" />
     </section>
+    <section class="secIniOfer">
+      <h1>
+        ¡PROMOCIONES HASTA EL 50 % DE DESCUENTO! ELIGE ENTRE MÁS DE 100
+        PRODUCTOS SELECCIONADOS DE ESTA TEMPORADA
+      </h1>
+    </section>
     <section class="secIni2">
       <img src="../assets/imSec2.png" />
     </section>
@@ -21,17 +27,33 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap");
 * {
   margin: 0;
 }
 body {
-  background-color: black;
+  background-color: white;
 }
 main {
   .secIni1 {
     img {
       width: 100%;
     }
+  }
+  .secIniOfer {
+    margin-top: 1.5rem;
+
+    display: flex;
+    height: auto;
+    background-color: #04724d;
+    color: black;
+    font-family: "Noto Sans", sans-serif;
+    h1 {
+      text-align: center;
+      width: 35rem;
+      padding: 2rem;
+    }
+    justify-content: center;
   }
   .secIni2 {
     margin-top: 2rem;
