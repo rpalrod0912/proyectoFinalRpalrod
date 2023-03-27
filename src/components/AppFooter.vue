9ffdde261d7c94e2c98cf2194fe9b652b8b6137e
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-03-27 18:26:45 +0200

    Terminando Footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,32 +1,44 @@
 <template>
   <footer>
-    <ul class="help">
-      <h1>AYUDA</h1>
-      <li>Preguntas Frecuentes</li>
-      <li>Estado de mi pedido</li>
-      <li>Envíos</li>
-    </ul>
-    <ul class="social">
-      <h1>REDES SOCIALES</h1>
-      <div class="socialDiv">
-        <li><img src="../assets/isntagramIcon.png" /></li>
-        <li><img src="../assets/youtubeIcon.png" /></li>
-        <li><img src="../assets/twitterIcon.png" /></li>
-      </div>
-    </ul>
-    <ul class="payment">
-      <h1>MÉTODOS DE PAGO</h1>
-      <div class="paymentDiv">
-        <li><img src="@/assets/paymentsIcons/visaIcon.png" /></li>
-        <li><img src="@/assets/paymentsIcons/masterCardIcon.png" /></li>
-        <li><img src="@/assets/paymentsIcons/amazonIcon.png" /></li>
-      </div>
-      <div class="paymentDiv payment2">
-        <li><img src="@/assets/paymentsIcons/paysafeIcon.png" /></li>
-        <li><img src="@/assets/paymentsIcons/appleIcon.png" /></li>
-        <li><img src="@/assets/paymentsIcons/paypalIcon.png" /></li>
-      </div>
-    </ul>
+    <section class="secFoot1">
+      <ul class="help">
+        <h1>AYUDA</h1>
+        <li>Preguntas Frecuentes</li>
+        <li>Estado de mi pedido</li>
+        <li>Envíos</li>
+      </ul>
+      <ul class="social">
+        <h1>REDES SOCIALES</h1>
+        <div class="socialDiv">
+          <li><img src="../assets/isntagramIcon.png" /></li>
+          <li><img src="../assets/youtubeIcon.png" /></li>
+          <li><img src="../assets/twitterIcon.png" /></li>
+        </div>
+      </ul>
+      <ul class="payment">
+        <h1>MÉTODOS DE PAGO</h1>
+        <div class="paymentDiv">
+          <li><img src="@/assets/paymentsIcons/visaIcon.png" /></li>
+          <li><img src="@/assets/paymentsIcons/masterCardIcon.png" /></li>
+          <li><img src="@/assets/paymentsIcons/amazonIcon.png" /></li>
+        </div>
+        <div class="paymentDiv payment2">
+          <li><img src="@/assets/paymentsIcons/paysafeIcon.png" /></li>
+          <li><img src="@/assets/paymentsIcons/appleIcon.png" /></li>
+          <li><img src="@/assets/paymentsIcons/paypalIcon.png" /></li>
+        </div>
+      </ul>
+    </section>
+    <div class="separador"></div>
+    <section class="secFoot2">
+      <ul>
+        <li>Política de Privacidad</li>
+        <li class="sepVertical">|</li>
+        <li>Condiciones de Compra</li>
+        <li class="sepVertical">|</li>
+        <li>Política de Cookies</li>
+      </ul>
+    </section>
   </footer>
 </template>
 <script>
@@ -35,7 +47,30 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-footer {
+ul {
+  list-style-type: none;
+}
+
+.sepVertical {
+  display: flex;
+  justify-items: center;
+  padding: 1px;
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 700;
+  color: #95a3a4;
+  width: 0.3rem;
+  background-color: #95a3a4;
+  justify-content: center;
+  text-shadow: 0px 0px 0px rgba(0, 0, 0, 0.25);
+}
+.separador {
+  border-top: 1px solid #dadada;
+  height: 2px;
+  padding: 0;
+  margin: 20px auto 0 auto;
+}
+.secFoot1 {
   display: flex;
   margin-top: 4rem;
   justify-content: space-around;
@@ -47,9 +82,7 @@ footer {
   img {
     width: 4rem;
   }
-  ul {
-    list-style-type: none;
-  }
+
   .help {
     li {
       margin-top: 0.5rem;
@@ -78,6 +111,25 @@ footer {
   }
   .payment2 {
     margin-top: 0.1rem;
+  }
+}
+
+.secFoot2 {
+  margin-top: 4rem;
+  margin-bottom: 3rem;
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ul {
+    display: flex;
+    align-items: center;
+
+    li {
+      margin: 1rem;
+    }
   }
 }
 </style>
