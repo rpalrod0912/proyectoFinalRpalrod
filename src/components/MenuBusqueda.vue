efab6cb960b3347445bbb9fb15b959cb569b1e00
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-03-29 22:29:18 +0200

    Mixings/Refactor

--- src/components/MenuBusqueda.vue
+++ src/components/MenuBusqueda.vue
@@ -83,103 +83,11 @@ export default {
 .bodyStyle {
   overflow: hidden;
 }
+@include estiloMenuBusqueda("#TopMenu__toggle", ".menu__btn", ".TopMenu__box");
 
-#TopMenu__toggle {
-  z-index: 1;
-  width: 1rem;
-  position: relative;
-  right: -1rem;
-  opacity: 0;
-}
-#TopMenu__toggle:checked + .menu__btn > span {
-  transform: rotate(45deg);
-}
-#TopMenu__toggle:checked + .menu__btn > span::before {
-  top: 0;
-  transform: rotate(0deg);
-  background-color: black;
-}
-#TopMenu__toggle:checked + .menu__btn > span::after {
-  top: 0;
-  transform: rotate(90deg);
-  background-color: black;
-  .greyContainer {
-    visibility: visible;
-  }
-}
-#TopMenu__toggle:checked ~ .TopMenu__box {
-  top: 0rem !important;
-  left: 0rem;
-  padding: 0rem;
-  .greyContainer {
-    top: 60% !important;
-    left: 0rem;
-    width: 100%;
-  }
-}
-
-.menu__btn {
-  position: fixed;
-
-  width: 26px;
-  height: 26px;
-  cursor: pointer;
-  z-index: 1;
-}
-.menu__btn > span,
-.menu__btn > span::before,
-.menu__btn > span::after {
-  display: block;
-  position: absolute;
-  width: 100%;
-  height: 2px;
-  background-color: white;
-  transition-duration: 0.25s;
-}
-.menu__btn > span::before {
-  content: "";
-  top: -8px;
-}
-.menu__btn > span::after {
-  content: "";
-  top: 8px;
-}
-.TopMenu__box {
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  top: -100%;
-  //width: 300px;
-  width: 100%;
-  height: 60%;
-  margin: 0;
-  padding: 80px 0;
-  list-style: none;
-  background-color: white;
-  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
-  transition-duration: 0.1s;
-  opacity: 1;
-  .greyContainer {
-    left: auto;
-    background-color: rgb(8 8 8);
-    position: fixed;
-    //Posicion de la que parte el fondo transparente, este caso cambiamos a arriba hacia abajo
-    top: -100%;
-    left: -100%;
-    width: 80%;
-    height: 300rem;
-    z-index: 1;
-    bottom: 0%;
-    opacity: 0.6;
-    transition-duration: 0.25s;
-  }
-}
 .TopMenu__item {
   padding: 12px 24px;
   margin-top: 0.5rem;
-
-  encabezado {
-  }
 }
 .textoPlano {
   @include fuenteSemiBold;
--- src/components/MenuGeneral.vue
+++ src/components/MenuGeneral.vue
@@ -63,6 +63,8 @@ export default {
 };
 </script>
 <style lang="scss">
+@import "../helpers/mixings.scss";
+
 $baseColor: white;
 $secondColor: black;
 
@@ -70,64 +72,7 @@ $secondColor: black;
   overflow: hidden;
 }
 
-.areaClick {
-  z-index: auto;
-  height: 60%;
-  width: 9%;
-  position: absolute;
-  left: 1vw;
-  top: 2vw;
-  cursor: pointer;
-}
-
-#menu__toggle {
-  z-index: 1;
-  width: 1rem;
-  position: relative;
-  right: -1rem;
-  opacity: 0;
-}
-#menu__toggle:checked + .menu__btn > span {
-  transform: rotate(45deg);
-}
-#menu__toggle:checked + .menu__btn > span::before {
-  top: 0;
-  transform: rotate(0deg);
-  background-color: $secondColor;
-}
-#menu__toggle:checked + .menu__btn > span::after {
-  top: 0;
-  transform: rotate(90deg);
-  background-color: $secondColor;
-  .greyContainer {
-    visibility: visible;
-  }
-}
-#menu__toggle:checked ~ .menu__box {
-  left: 0 !important;
-  .greyContainer {
-    left: 20% !important;
-  }
-}
-
-.menu__btn {
-  position: fixed;
-
-  width: 26px;
-  height: 26px;
-  cursor: pointer;
-  z-index: 1;
-}
-.menu__btn > span,
-.menu__btn > span::before,
-.menu__btn > span::after {
-  display: block;
-  position: absolute;
-  width: 100%;
-  height: 2px;
-  background-color: var(--baseColor, $baseColor);
-  transition-duration: 0.25s;
-}
+@include estiloMenus("#menu__toggle", ".menu__btn", ".menu__box");
 
 //ESTA CLASE SOLO PONDRA EL SPAN NEGRO CUANDO TENGAMOS EL HEADER SECUNDARIO DEL MENU ABIERTO
 .blackSpan {
@@ -138,42 +83,6 @@ $secondColor: black;
   }
 }
 
-.menu__btn > span::before {
-  content: "";
-  top: -8px;
-}
-.menu__btn > span::after {
-  content: "";
-  top: 8px;
-}
-.menu__box {
-  display: block;
-  position: fixed;
-  top: 0;
-  left: -100%;
-  //width: 300px;
-  width: 20%;
-  height: 100%;
-  margin: 0;
-  padding: 80px 0;
-  list-style: none;
-  background-color: $baseColor;
-  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
-  transition-duration: 0.25s;
-  opacity: 1;
-  .greyContainer {
-    left: auto;
-    background-color: $secondColor;
-    position: fixed;
-    left: -100%;
-    width: 80%;
-    height: 300rem;
-    z-index: 1;
-    bottom: 0%;
-    opacity: 0.6;
-    transition-duration: 0.25s;
-  }
-}
 .menu__item {
   display: block;
   padding: 12px 24px;
