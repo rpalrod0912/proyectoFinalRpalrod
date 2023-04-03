eb43911462138af0a396297282d455c115b218c9
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-04-03 23:46:54 +0200

    login Menu retocado

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,8 +1,18 @@
 <template>
   <section class="loginWindow">
     <label for="loginPasswordInp">
-      <h1>INTRODUCE TU CONTRASEÑA</h1>
+      <h1 v-if="this.loginMode === 'sms' || this.loginMode === 'mail'">
+        CÓDIGO DE VERIFICACÓN
+      </h1>
+      <h1 v-else>INTRODUCE TU CONTRASEÑA</h1>
     </label>
+    <p v-if="this.loginMode === 'sms'" class="pDescrip">
+      Introduce el código que te hemos enviado al SMS al número de teléfono
+      +34XXXXXXXX
+    </p>
+    <p v-else-if="this.loginMode === 'mail'" class="pDescrip">
+      Introduce el código que te hemos enviado por e-mail a ********02@gmail.com
+    </p>
     <div class="passwordInput">
       <input type="password" placeholder="Contraseña" id="loginInput" />
 
@@ -19,14 +29,32 @@
     </div>
     <ButtonComponent class="btn" msj="SIGUIENTE"></ButtonComponent>
     <ButtonComponent
+      v-if="this.loginMode !== 'password'"
       class="btn"
+      msj="No he recibido el código"
+    ></ButtonComponent>
+    <ButtonComponent
+      v-if="this.loginMode === 'password'"
+      class="colorBtnSoc btn"
       msj="Acceder con código vía SMS"
     ></ButtonComponent>
     <ButtonComponent
-      class="btn"
+      v-if="this.loginMode === 'password'"
+      class="colorBtnSoc btn"
       msj="Acceder con código vía e-mail"
     ></ButtonComponent>
-    <p class="underLineTxt">Restablecer Contraseña</p>
+    <p v-if="this.loginMode === 'password'" class="underLineTxt">
+      Restablecer Contraseña
+    </p>
+    <p v-else-if="this.loginMode !== 'password'" class="underLineTxt">
+      Acceder con contraseña
+    </p>
+    <p v-if="this.loginMode === 'sms'" class="underLineTxt">
+      Acceder con código vía e-mail
+    </p>
+    <p v-if="this.loginMode === 'mail'" class="underLineTxt">
+      Acceder con código vía SMS
+    </p>
   </section>
 </template>
 <script>
@@ -35,12 +63,14 @@ import ButtonComponent from "./ButtonComponent.vue";
 export default {
   created() {
     this.modoTexto = false;
+    this.loginMode = this.loginModeProp;
   },
   data() {
     return {
       modoTexto: null,
       mostrarTexto: require("../assets/SeeThrough.png"),
       ocultarTexto: require("../assets/blindIcon.png"),
+      loginMode: null,
     };
   },
   name: "LoginApp",
@@ -57,10 +87,28 @@ export default {
     },
   },
   components: { ButtonComponent },
+  props: {
+    loginModeProp: String,
+  },
 };
 </script>
 <style lang="scss">
 @import "../helpers/mixings.scss";
+.colorBtnSoc {
+  background-color: #dadada;
+  border: 1px white;
+  p {
+    color: black;
+  }
+}
+.pDescrip {
+  font-size: 1.5rem;
+  width: 70%;
+  text-align: center;
+  color: black;
+  @include fuenteRegular;
+  padding-bottom: 2rem;
+}
 .loginWindow {
   display: flex;
   position: relative;
--- src/views/LoginApp.vue
+++ src/views/LoginApp.vue
@@ -1,7 +1,12 @@
 <template>
   <WhiteHeader tipo="Dark"> </WhiteHeader>
   <main>
-    <LoginComponent></LoginComponent>
+    <!--
+      loginModeProp="password"//Muestra Pantalla con contraseña
+      loginModeProp="sms"//Muestra Pantalla con acceso codigo sms
+      loginModeProp="mail"//Muestra Pantalla con acceso codigo movil
+    -->
+    <LoginComponent loginModeProp="mail"></LoginComponent>
   </main>
 </template>
 <script>
@@ -9,8 +14,12 @@ import LoginComponent from "@/components/LoginComponent.vue";
 import WhiteHeader from "@/components/WhiteHeader.vue";
 
 export default {
+  /*eslint-disable */
   name: "LoginApp",
   components: { WhiteHeader, LoginComponent },
+  props: {
+    //Two Login Mode depends of user input, password or SMS code or mail code
+  },
 };
 </script>
 <style lang="scss" scoped>
