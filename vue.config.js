28a525f65352c438e57f8ed297c5f8d5021e42e2
PROD\rafael.palomino <rafael.palomino@alten.es>
2023-05-31 14:32:11 +0200

    Now App is available as PWA

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
+const { GenerateSW } = require("workbox-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
     //IMPORTANTE DE LO CONTRARIO AL HACER BUILD EL PLUGIN AÑADIRA ESTILOS INDESEADOS
     extract: false,
+  },
+  pwa: {
+    name: "StreetUp",
+    themeColor: "#242424",
+  },
+  configureWebpack: {
+    plugins: [new GenerateSW()],
   },
 });
