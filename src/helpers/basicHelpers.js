abb2b13e3b66e93d03f4fa9146677d5ac85ee71c
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-06-17 15:52:11 +0200

    Changed API_URL

--- src/helpers/basicHelpers.js
+++ src/helpers/basicHelpers.js
@@ -1,7 +1,9 @@
 /*eslint-disable */
 
 //const API_URL = "http://localhost:8080/";
-const API_URL = "https://rpalrodpostgreapi.onrender.com/";
+//const API_URL = "https://rpalrodpostgreapi.onrender.com/";
+//Cambio a railway para un mejor deslpliegue del Back End
+const API_URL="https://porjectofinalpostgre-production-3a0c.up.railway.app/";
 
 function toHome() {
   this.$router.push("/");
